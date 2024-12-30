6aa1a8a7ae2c71a479a9584b6d5f5b991cf23339
Dasunika0216 <dasunikamd.22@uom.lk>
2024-12-30 22:34:31 +0530

    Done with the collection page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useContext } from 'react';
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
 
 const Product = () => {
-  return (
-    <div>
-      
+  const {productId} = useParams();
+  // console.log(productId);
+
+  const {products, currency} = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState('');
+  const [size, setSize] = useState('');
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        // console.log(item);
+        setImage(item.image[0]);
+        return null;
+      }
+    })
+  }
+
+  useEffect(() => {
+    fetchProductData();
+  },[productId, products])
+
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+
+      {/* ------------ Product data ----------------*/}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+
+        {/* ---------------- Product images -----------------*/}
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item, index) => (
+                <img src={item} alt="" key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' onClick={() => setImage(item)}/>
+              ))
+            }
+          </div>
+          <div className='w-full sm:w-[80%]'>
+            <img src={image} alt="" className='w-full h-auto' />
+          </div>
+        </div>
+
+        {/* ---------------- Product information ------------------------*/}
+        <div className='flex-1'>
+          <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+          <div className='flex items-center gap-1 mt-2'>
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+            <p className='pl-2'>(122)</p>
+          </div>
+          <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+          <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+          <div className='flex flex-col gap-4 my-8'>
+            <p>Select Size</p>
+            <div className='flex gap-2 '>
+              {productData.sizes.map((item, index) => (
+                <button key={index} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} onClick={() => setSize(item)}>
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5'/>
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+            <p>100% Authentic and Verified Products.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Secure Payments with Multiple Payment Options Available.</p>
+          </div>
+        </div>
+      </div>
+
+      {/* -------------- Description & Review section ------------------ */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p >E-commerce websites allow businesses to sell products or services online, offering a convenient and accessible platform for customers to make purchases. These websites typically feature product listings with descriptions, images, prices, and variations like size or color, making it easier for customers to browse and select items.</p>
+          <p>Additionally, e-commerce platforms help businesses manage inventory, track orders, and process payments securely. They often include features like personalized recommendations, discounts, and customer reviews, enhancing the shopping experience and encouraging customer loyalty.</p>
+        </div>
+      </div>
     </div>
-  )
+  ) : <div className='opacity-0'></div>
 }
 
 export default Product
