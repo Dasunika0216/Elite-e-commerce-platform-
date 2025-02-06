c46cecb2f508632cc176596fd021da621bf504c0
Dasunika0216 <dasunikamd.22@uom.lk>
2025-02-06 13:55:58 +0530

    Add cart functionality with routes and authentication middleware

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,16 @@
+// Add products to user's cart
+const addToCart = async (req, res) => {
+
+};
+
+// Update user's cart
+const updateCart = async (req, res) => {
+    
+};
+
+// Get user's cart data
+const getUserCart = async (req, res) => {
+    
+};
+
+export { addToCart, updateCart, getUserCart };
