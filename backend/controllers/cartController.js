d6e26718589b5c8e9f7c1a11696296e7f1fd49eb
Dasunika0216 <dasunikamd.22@uom.lk>
2025-02-06 14:18:53 +0530

    Implement addToCart functionality and cart data management

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,6 +1,33 @@
+import userModel from "../models/userModel";
+
 // Add products to user's cart
 const addToCart = async (req, res) => {
+    try {
+        const {userId, itemId, size} = req.body;
 
+        const userdata = await userModel.findById(userId);
+        let cartData = await userdata.cartData;
+
+        if (cartData[itemId]){
+            if (cartData[itemId][size]){
+                cartData[itemId][size] += 1;
+            }
+            else{
+                cartData[itemId][size] = 1;
+            }
+        }
+        else{
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+
+        await userModel.findByIdAndUpdate(userId, {cartData});
+        res.json({success:true, message:"Item added to cart!"});
+    } 
+    catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message});
+    }
 };
 
 // Update user's cart
