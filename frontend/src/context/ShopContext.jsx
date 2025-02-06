921f75fcc93660a1a02e21054a65fe86f8b1e3ce
Dasunika0216 <dasunikamd.22@uom.lk>
2025-02-06 20:49:49 +0530

    Add cart update and retrieval functionality in ShopContext

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -72,6 +72,16 @@ const ShopContextProvider = (props) => {
         let cartData = structuredClone(cartItems);
         cartData[itemId][size] = quantity;
         setCartItems(cartData);
+
+        if (token) {
+            try {
+                await axios.post(backendUrl + '/api/cart/update', {itemId, size, quantity}, {headers:{token}});
+            } 
+            catch (error) {
+                console.log(error);
+                toast.error(error.message);
+            }
+        }
     }
 
     const getCartAmount = () => {
@@ -109,7 +119,20 @@ const ShopContextProvider = (props) => {
             console.log(error);
             toast.error(error.message);
         }
-    }
+    };
+
+    const getUserCart = async (token) => {
+        try {
+            const response = await axios.post(backendUrl + '/api/cart/get', {}, {headers:{token}});
+            if (response.data.success) {
+                setCartItems(response.data.cartData);
+            }
+        } 
+        catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    };
 
     useEffect(() => {
         getProductData();
@@ -118,6 +141,7 @@ const ShopContextProvider = (props) => {
     useEffect(() => {
         if (!token && localStorage.getItem('token')){
             setToken(localStorage.getItem('token'));
+            getUserCart(localStorage.getItem('token'));
         }
     },[]);
 
