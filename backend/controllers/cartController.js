78297c9e7c7c9b70ba2b0e7acb2996f93f604795
Dasunika0216 <dasunikamd.22@uom.lk>
2025-02-06 18:04:00 +0530

    Implement updateCart functionality

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -32,12 +32,31 @@ const addToCart = async (req, res) => {
 
 // Update user's cart
 const updateCart = async (req, res) => {
-    
+    try {
+        const {userId, itemId, size, quantity} = req.body;
+
+        const userdata = await userModel.findById(userId);
+        let cartData = await userdata.cartData;
+
+        cartData[itemId][size] = quantity;
+
+        await userModel.findByIdAndUpdate(userId, {cartData});
+        res.json({success:true, message:"Updated successfully!"});
+    } 
+    catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message});
+    }
 };
 
 // Get user's cart data
 const getUserCart = async (req, res) => {
-    
+    try {
+        
+    } 
+    catch (error) {
+        
+    }
 };
 
 export { addToCart, updateCart, getUserCart };
