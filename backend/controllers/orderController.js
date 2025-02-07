cc715e7ad1c9119a70893ea262111f2dd8667e87
Dasunika0216 <dasunikamd.22@uom.lk>
2025-02-07 20:11:13 +0530

    Implement order placement functionality with cash on delivery method

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,31 @@
+import orderModel from '../models/orderModel.js';
+import userModel from '../models/userModel.js';
+
 // Placing orders using cash on delivery method
 const placeOrder = async (req, res) => {
+    try {
+        const {userId, items, amount, address} = req.body;
+        const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod: "COD",
+            payment: false,
+            date: Date.now()
+        } 
 
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();          // New order is saved in the database
+
+        await userModel.findByIdAndUpdate(userId, {cartData: {} });  // Cart data is cleared after placing the order
+
+        res.json({success: true, message: "Order placed successfully!"});
+    } 
+    catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
 }
 
 // Placing orders using Stripe method
