216f216094faff53de9dee61abb173dce39c3312
Dasunika0216 <dasunikamd.22@uom.lk>
2025-03-05 15:19:37 +0530

    Add Stripe payment verification in orderController

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -92,6 +92,28 @@ const placeOrderStripe = async (req, res) => {
     }
 }
 
+//Verify stripe payment
+const veriftStripe = async (req, res) => {
+    const {orderId, success, userId} = req.body;
+
+    try {
+        if (success === "true") {
+            await orderModel.findByIdAndUpdate(orderId, {payment: true}); // Payment is verified.
+            await userModel.findByIdAndUpdate(userId, {cartData: {} }); // Cart data is cleared after placing the payment.
+
+            res.json({success: true});
+        }
+        else{
+            await orderModel.findByIdAndDelete(orderId); // Order is deleted if payment is not successful.
+            res.json({success: false});
+        }
+    } 
+    catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
+}
+
 // Placing orders using Razorpay method
 const placeOrderRazorpay = async (req, res) => {
    
